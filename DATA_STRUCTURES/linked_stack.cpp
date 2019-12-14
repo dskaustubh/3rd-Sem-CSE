be721862f08a9cbb6fffdbf5d1c8ed087f4d5810
Kaustubha DS <dskaustubh@gmail.com>
2019-12-14 19:32:40 +0530

    linked stack complete

--- DATA_STRUCTURES/linked_stack.cpp
+++ DATA_STRUCTURES/linked_stack.cpp
@@ -33,17 +33,53 @@ void display(int i)
         temp = temp->link;
     }
 }
+int pop(int i)
+{
+    int r; 
+    nodeptr temp=top[i];
+    if(!top[i])
+    {
+        cout<<"\n Stack Empty Cannot POP ";
+        exit(0);
+    }
+    r=temp->data;
+    top[i]=top[i]->link;
+    free(temp);
+    return r;
+}
 int main()
 {
-    int i;
+    int i,ch,v;
     for (i = 0; i < 10; i++)
     {
         top[i] = nullptr;
     }
-    push(1, 1);
-    display(1);
-    push(1, 2);
-    display(1);
-    push(1, 3);
-    display(1);
+    do
+    {
+        cout<<"\n 1 To push \n 2 to pop \n 3 to display \n 4 to exit \n Enter your choice: ";
+        cin>>ch;
+        switch(ch)
+        {
+            case 1:
+                    cout<<"\nEnter the Stack no : ";
+                    cin>>i;
+                    cout<<"\n Enter data to be Inserted: ";
+                    cin>>v;
+                    push(i,v);
+                    break;
+            case 2:
+                    cout<<"\nEnter the Stack no : ";
+                    cin>>i;
+                    v=pop(i);
+                    cout<<"\n Succesfully popped "<<v;
+                    break;
+            case 3:
+                    cout<<"\n Enter THe Stack no: ";
+                    cin>>i;
+                    display(i);
+                    break;
+            default:exit(0);
+        }
+    } while (ch!=4);
+    
 }
